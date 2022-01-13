690f5e9f167e6e775b2e54c7e160cd0fa5b0bc36
cammylim <s10196467@connect.np.edu.sg>
2022-01-13 20:59:27 +0800

    Initial Commit

---
+++ MAD2AssignmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD2AssignmentApp
+//
+//  Created by Cammy Lim on 13/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
