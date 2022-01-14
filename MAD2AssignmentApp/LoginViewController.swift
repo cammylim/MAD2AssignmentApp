56016fe12689594d30b3f6854aeb9baacd693611
YuxuanWong <s10196840@connect.np.edu.sg>
2022-01-14 08:28:55 +0800

    implement login page (incomplete)

---
+++ MAD2AssignmentApp/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  MAD2AssignmentApp
+//
+//  Created by Yuxuan on 13/1/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
