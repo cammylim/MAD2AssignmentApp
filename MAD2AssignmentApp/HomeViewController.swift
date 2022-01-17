d3a3b398f1c81a00f918a57aafa08815ed44bd56
cammylim <s10196467@connect.np.edu.sg>
2022-01-17 22:25:15 +0800

    homepage added (incomplete)

---
+++ MAD2AssignmentApp/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  MAD2AssignmentApp
+//
+//  Created by Cammy Lim on 17/1/22.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "home-bg.svg")!);
+
+    }
+    
+}
