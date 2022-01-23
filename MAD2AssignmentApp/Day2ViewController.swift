372d834893f642e2292e53d8ccdca26a61b17dee
cammylim <s10196467@connect.np.edu.sg>
2022-01-23 15:43:09 +0800

    tag view shows selected

--- MAD2AssignmentApp/Day2ViewController.swift
+++ MAD2AssignmentApp/Day2ViewController.swift
@@ -10,30 +10,40 @@ import UIKit
 
 class Day2ViewController:UIViewController, TagListViewDelegate{
     
-    var tagList:[String]?
+    var tagList:[String]!
     var tag:String?
-    var tagView:TagView?
+    var tagView:TagView!
     @IBOutlet weak var tagListView: TagListView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "diary-bg.svg")!);
+        tagListView.delegate = self
         tagListView.textFont = UIFont(name: "BalsamiqSans-Regular", size: 16)!
+
         setTags()
     }
+    
     func setTags(){
         tagList = ["Shopping", "Exercise", "Gardening"]
         var i = 0
         while i < tagList!.count {
+            tagView = tagListView.addTag(tagList[i])
+            self.tagListView.tagPressed(tagView)
+            
+            tagView.isSelected = false
             i += 1
-            tagView = tagListView.addTag(tagList![i])
-            tagView!.onTap = {[weak self] tagView in
-                self?.tagListView.tagPressed(tagView)
-            }
         }
+        
+        
     }
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
     func tagPressed(_ title: String, tagView: TagView, sender: TagListView) {
-            print("Tag pressed: \(title), \(sender)")
+        print("Tag pressed: \(title), \(sender)")
+        tagView.isSelected = !tagView.isSelected
         }
     func tagRemoveButtonPressed(_ title: String, tagView: TagView, sender: TagListView) {
             print("Tag Remove pressed: \(title), \(sender)")
