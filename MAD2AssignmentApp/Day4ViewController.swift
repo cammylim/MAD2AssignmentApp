6c6ec596d81faaacc27a67e30170ed336d472941
YuxuanWong <s10196840@connect.np.edu.sg>
2022-01-25 01:33:30 +0800

    add diary entry 4, add placeholder for diary entry 3

---
+++ MAD2AssignmentApp/Day4ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Day4ViewController.swift
+//  MAD2AssignmentApp
+//
+//  Created by Yuxuan on 25/1/22.
+//
+
+import Foundation
+import UIKit
+
+class Day4ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "diary-bg.svg")!);
+    }
+    
+}
