fcb5eb607d90eb4ed90d31f46769eed80dd231f7
YuxuanWong <s10196840@connect.np.edu.sg>
2022-01-27 19:32:16 +0800

    changed text field to text view in day4

--- MAD2AssignmentApp/Day4ViewController.swift
+++ MAD2AssignmentApp/Day4ViewController.swift
@@ -8,17 +8,41 @@
 import Foundation
 import UIKit
 
-class Day4ViewController: UIViewController {
+class Day4ViewController: UIViewController, UITextViewDelegate {
     
     var selectedDate: Date?
     var diary: Diary?
     @IBOutlet weak var titleField: UITextField!
-    @IBOutlet weak var bodyField: UITextField!
+//    @IBOutlet weak var bodyField: UITextField!
+    @IBOutlet weak var bodyField: UITextView!
     let diaryDAL: DiaryDataAccessLayer = DiaryDataAccessLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "diary-bg.svg")!);
+        bodyField.delegate = self
+        bodyField.text = "Add body here"
+        bodyField.textColor = UIColor.systemGray3
+        bodyField.textContainerInset = UIEdgeInsets(top: 8, left: 5, bottom: 8, right: 5)
+        bodyField.layer.cornerRadius = 6
+        bodyField.layer.masksToBounds = true
+        bodyField.layer.borderColor = UIColor(red: 0.933, green: 0.933, blue: 0.933, alpha: 1).cgColor
+        bodyField.layer.borderWidth = CGFloat(1)
+    }
+    
+    // modify body text field (text view)
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if bodyField.textColor == UIColor.systemGray3 {
+            bodyField.text = nil
+            bodyField.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if bodyField.text.isEmpty {
+            bodyField.text = "Add body here"
+            bodyField.textColor = UIColor.systemGray3
+        }
     }
     
     // hide keyboard when tap on empty space
